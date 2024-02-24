304db180ea39f4746b09ee3bb9f5539c77e13266
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-24 17:16:17 -0500

    Remove code, reeval thought

--- script.js
+++ script.js
@@ -2,17 +2,7 @@ let userInput = [];
 const inputNumber = document.getElementById("input-number");
 const convertButton = document.getElementById("convert-button");
 const firstSection = document.getElementById("first-section");
-const secondSection = document.getElementById("second-section");
-const thirdSection = document.getElementById("third-section");
 let meterFootConversion = `
-      ${userInput} meters = ${userInput[i] * 3.28084} feet  |  
-      ${userInput[i] * 3.28084} feet = ${userInput} meters
-      `;
-let literGallonConversion = `
-      ${userInput} meters = ${userInput[i] * 3.28084} feet  |  
-      ${userInput[i] * 3.28084} feet = ${userInput} meters
-      `;
-let KiloPoundConversion = `
       ${userInput} meters = ${userInput[i] * 3.28084} feet  |  
       ${userInput[i] * 3.28084} feet = ${userInput} meters
       `;
@@ -26,11 +16,6 @@ function renderInput() {
   let inputItem = "";
   for (let i = 0; i < userInput.length; i++) {
     inputItem += meterFootConversion;
-    inputItem += literGallonConversion;
-    inputItem += KiloPoundConversion;
-    
-
   }
   firstSection.innerHTML = inputItem;
-  second
 }

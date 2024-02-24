268f89608f30adc8f712261086e5e09b3fdc387b
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-24 12:08:21 -0500

    Push user value to input array

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const inputNumber = document.getElementById("input-number");
 const convertButton = document.getElementById("convert-button");
 const firstSection = document.getElementById("first-section");
 
-convertButton.addEventListener("click", function () {});
+convertButton.addEventListener("click", function () {
+  userInput.push(inputNumber.value);
+  renderInput();
+});
 
 function renderInput() {
+  let inputItem = "";
   for (let i = 0; i < userInput.length; i++) {
-    firstSection += userInput[i];
+    inputItem += userInput[i];
   }
+  firstSection.innerHTML = inputItem;
 }

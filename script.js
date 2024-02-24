b6d5852773673d4460bb64a789421b69e1f9a6a2
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-24 15:26:09 -0500

    Added meter/foot variable

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let userInput = [];
 const inputNumber = document.getElementById("input-number");
 const convertButton = document.getElementById("convert-button");
 const firstSection = document.getElementById("first-section");
+let meterFootConversion = `
+      ${userInput} meters = ${userInput[i] * 3.28084} feet  |  
+      ${userInput[i] * 3.28084} feet = ${userInput} meters
+      `;
 
 convertButton.addEventListener("click", function () {
   userInput.push(inputNumber.value);
@@ -11,10 +15,7 @@ convertButton.addEventListener("click", function () {
 function renderInput() {
   let inputItem = "";
   for (let i = 0; i < userInput.length; i++) {
-    inputItem += `
-      ${userInput} meters = ${userInput[i] * 3.28084} feet  |  
-      ${userInput[i] * 3.28084} feet = ${userInput} meters
-      `;
+    inputItem += meterFootConversion;
   }
   firstSection.innerHTML = inputItem;
 }

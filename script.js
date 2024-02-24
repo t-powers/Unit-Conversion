937ca95ad0f33effb541c07999072ee2b7c63344
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-24 11:27:55 -0500

    Removing and redoing

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-let userInput = ""
+let userInput = [];
 const inputNumber = document.getElementById("input-number");
 const convertButton = document.getElementById("convert-button");
 const firstSection = document.getElementById("first-section");
-
-convertButton.addEventListener("click", function(){
-    
-}

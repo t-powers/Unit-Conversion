972a414dd1ee664dbdbaa348f157afde906bcdf7
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-24 12:01:49 -0500

    Add eventlistener and renderinput fx

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let userInput = [];
 const inputNumber = document.getElementById("input-number");
 const convertButton = document.getElementById("convert-button");
 const firstSection = document.getElementById("first-section");
+
+convertButton.addEventListener("click", function () {});
+
+function renderInput() {
+  for (let i = 0; i < userInput.length; i++) {
+    firstSection += userInput[i];
+  }
+}

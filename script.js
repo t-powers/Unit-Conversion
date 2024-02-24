beb7d595426bdeac3ae3b13a50a9beda2b061259
t-powers <127955989+t-powers@users.noreply.github.com>
2024-02-24 10:49:38 -0500

    Added first section connection

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const inputNumber = document.getElementById("input-number");
 const convertButton = document.getElementById("convert-button");
+const firstSection = document.getElementById("first-section")
 
+convertButton.addEventListener("click", function () {
+  console.log(inputNumber.value);
+});

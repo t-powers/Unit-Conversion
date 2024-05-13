e3903c40da5308b3f64b0a089b961e45b9b3c2d4
t-powers <127955989+t-powers@users.noreply.github.com>
2024-05-13 07:38:19 -0400

    Adding conversions calcs

--- script.js
+++ script.js
@@ -8,16 +8,21 @@ convertBtn.addEventListener("click", function () {
   massConversion();
 });
 
+// multiply meter to feet by 3.281
 function lengthConversion() {
   const length = document.getElementById("first-section");
-  length.innerHTML = inputNumber.value;
+  let meterInput = inputNumber.value;
+  let meterToFeet = meterInput * 3.281;
+  length.innerHTML = meterToFeet;
 }
 
+// divide liter to gallon by 3.785
 function volumeConversion() {
   const volume = document.getElementById("second-section");
   volume.innerHTML = inputNumber.value;
 }
 
+// multiply kilograms to pounds by 2.205
 function massConversion() {
   const mass = document.getElementById("third-section");
   mass.innerHTML = inputNumber.value;

df067125b5f3c3eb1660f9e5b1c135769ce69742
t-powers <127955989+t-powers@users.noreply.github.com>
2024-05-12 20:38:33 -0400

    Added page variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const length = document.getElementById("first-section").length;
 
+const volume = document.getElementById("second-section").length;
+
+const mass = document.getElementById("third-section").length;
+
+const convertBtn = document.getElementById("convert-button");
+
+let inputNumber = document.getElementById("input-number");

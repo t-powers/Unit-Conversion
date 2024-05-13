8d1af06a9b72d8255038a00443d37faaaf58d05d
t-powers <127955989+t-powers@users.noreply.github.com>
2024-05-12 21:07:59 -0400

    Created functions for volume/mass, code cleanup

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const volume = document.getElementById("second-section");
-
-const mass = document.getElementById("third-section");
-
 const convertBtn = document.getElementById("convert-button");
 
 let inputNumber = document.getElementById("input-number");
 
 convertBtn.addEventListener("click", function () {
   lengthConversion();
-  volume.innerHTML = inputNumber.value;
-  mass.innerHTML = inputNumber.value;
+  volumeConversion();
+  massConversion();
 });
 
 function lengthConversion() {
   const length = document.getElementById("first-section");
   length.innerHTML = inputNumber.value;
+}
+
+function volumeConversion() {
+  const volume = document.getElementById("second-section");
+  volume.innerHTML = inputNumber.value;
+}
+
+function massConversion() {
+  const mass = document.getElementById("third-section");
+  mass.innerHTML = inputNumber.value;
 }

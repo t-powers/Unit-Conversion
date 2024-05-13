e29911b1fa2b890bb1da4474883b3e39c314ef7d
t-powers <127955989+t-powers@users.noreply.github.com>
2024-05-13 19:47:40 -0400

    Completed functions

--- script.js
+++ script.js
@@ -13,18 +13,30 @@ function lengthConversion() {
   const length = document.getElementById("first-section");
   let meterInput = inputNumber.value;
   let meterToFeet = meterInput * 3.281;
-  let feetToMeter = meterInput / 3.281
-  length.innerHTML = `${meterInput} meters = ${meterToFeet.toFixed(3)} feet  |  ${meterInput} feet = ${feetToMeter.toFixed(3)} meters`;
+  let feetToMeter = meterInput / 3.281;
+  length.innerHTML = `${meterInput} meters = ${meterToFeet.toFixed(
+    3
+  )} feet  |  ${meterInput} feet = ${feetToMeter.toFixed(3)} meters`;
 }
 
 // divide liter to gallon by 3.785
 function volumeConversion() {
   const volume = document.getElementById("second-section");
-  volume.innerHTML = inputNumber.value;
+  let volumeInput = inputNumber.value;
+  let literToGallon = volumeInput / 3.785;
+  let gallonToLiter = volumeInput * 3.785;
+  volume.innerHTML = `${volumeInput} liters = ${literToGallon.toFixed(
+    3
+  )} gallons  |  ${volumeInput} gallons = ${gallonToLiter.toFixed(3)} liters`;
 }
 
 // multiply kilograms to pounds by 2.205
 function massConversion() {
   const mass = document.getElementById("third-section");
-  mass.innerHTML = inputNumber.value;
+  let massInput = inputNumber.value;
+  let kiloToPounds = massInput * 2.205;
+  let poundsToKilo = massInput / 2.205;
+  mass.innerHTML = `${massInput} kilograms = ${kiloToPounds.toFixed(
+    3
+  )}  |  ${massInput} pounds = ${poundsToKilo} kilograms`;
 }
